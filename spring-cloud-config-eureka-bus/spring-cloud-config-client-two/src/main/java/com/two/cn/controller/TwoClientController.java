766f053aea83d91e7eac9be2dd1e03ba81f914c3
MayBeCare <maybecare@yeah.net>
2018-05-02 16:49:40 +0800

    add spring-cloud-bus + RabbitMQ

---
+++ spring-cloud-config-eureka-bus/spring-cloud-config-client-two/src/main/java/com/two/cn/controller/TwoClientController.java
@@ -0,0 +1,26 @@
+package com.two.cn.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : asus
+ * @version : 1.0
+ * @Description:
+ * @Copyright : Copyright (c) 2018 All Rights Reserved
+ * @date :2018/5/02 14:27
+ */
+@RestController
+@RefreshScope        //使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
+public class TwoClientController {
+
+    @Value("${neo.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
---
+++ spring-cloud-config-eureka-bus/spring-cloud-config-client-two/src/test/java/com/two/cn/SpringCloudConfigClientTwoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.two.cn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigClientTwoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
